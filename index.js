516f2461768f167f64dcd24042ec8127031c7127
jfjmartin <jeremyfmartin@gmail.com>
2020-04-27 20:22:53 +0800

    nearly finished

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ function watchForm (){
        fetch(`https://dog.ceo/api/breed/${name}/images/random`)
        .then(response => response.json())
        .then(responseJson => renderPhoto(responseJson))
+       .catch(error => alert('Something went wrong. Try again later.'));
    }
 function renderPhoto(object){
     $('.picture').html(`
     <h1>Your dog</h1>
-    <img src=${object.message} alt="hmmm">`)
+    <img src=${object.message} alt="Hmmm. Not Sure Why That's Not Loading.....">`)
 }
 
 
